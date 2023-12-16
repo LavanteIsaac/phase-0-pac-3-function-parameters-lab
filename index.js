14d16f9338bbc7d1b4722ebf6ce0d9644599617a
Lavante Cotton <lavanteisaac@gmail.com>
2023-12-16 17:22:13 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log ("hello")
+function introduction (name){
+return `Hi, my name is ${name}.`
+
+
+}
+
+function introductionWithLanguage(name, language){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+
+
+}
+
+
+
+function introductionWithLanguageOptional(name, language= "JavaScript"){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+
+
+}
